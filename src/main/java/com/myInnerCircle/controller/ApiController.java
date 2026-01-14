b81befa690443ebaa10281876b51db4408f2ae92
warau <waraukuma94@mail.com>
2026-01-14 10:19:55 +0900

    No swagger

---
+++ src/main/java/com/myInnerCircle/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.myInnerCircle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/api")
+    public String test() {
+        return "Test OK";
+    }
+}
--- src/main/java/com/myInnerCircle/controller/HomeCotroller.java
+++ src/main/java/com/myInnerCircle/controller/HomeCotroller.java
@@ -2,20 +2,11 @@ package com.myInnerCircle.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class TestController {
-
+public class HomeCotroller {
     @GetMapping("/")
     public String home() {
         return "index";
-    }
-
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
-
-        return "Test OK";
     }
 }
